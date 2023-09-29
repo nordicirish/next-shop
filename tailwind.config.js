47549a1b91adbfc6e1e9612811598cca94f16466
Roger Graham <roger.graham@tuni.fi>
2023-09-29 15:13:25 +0300

    Add: content paths purge surplus styles from production bundle

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    // Example content paths...
+    "./public/**/*.html",
+    "./src/**/*.{js,jsx,ts,tsx,vue}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
