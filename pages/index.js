d3172b609d1e2c2138f37d51c400782577f7e94c
Roger Graham <roger.graham@tuni.fi>
2023-10-01 17:01:20 +0300

    Add: test products array

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,17 @@
 import Head from "next/head";
 import Title from "../components/Title";
+const products = [
+  {
+    id: 1,
+    title: "Product 1",
+  },
+  {
+    id: 2,
+    title: "Product 2",
+  },
+];
 function HomePage() {
+  console.log("[HomePage] render", products);
   return (
     <>
       <Head>
@@ -8,6 +19,11 @@ function HomePage() {
       </Head>
       <main className="px-6 py-4">
         <Title>Next Shop</Title>
+        <ul>
+          {products.map((product) => (
+            <li key={product.id}>{product.title}</li>
+          ))}
+        </ul>
       </main>
     </>
   );
