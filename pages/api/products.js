485a3a9bd8068d00cd6e8e58e09055894f16b624
Roger Graham <roger.graham@tuni.fi>
2023-10-01 19:32:03 +0300

    Add option to use client side api routing

---
+++ pages/api/products.js
@@ -0,0 +1,8 @@
+
+// use if client side rendering is needed
+// import { getProducts } from "@/lib/products";
+// export default async function handler(req, res) {
+//   console.log("[api/products] handler");
+//   const products = await getProducts();
+//   res.status(200).json(products);
+// }
--- pages/index_cs_api_route.js
+++ pages/index_cs_api_route.js
@@ -1,18 +1,30 @@
 import Head from "next/head";
 import Title from "../components/Title";
 import { getProducts } from "../lib/products";
+import { useEffect, useState } from "react";
+
 export async function getStaticProps() {
   const products = await getProducts();
   return {
     props: {
       products,
     },
-    //incremental static regeneration only works in production
+    //incrmental static regeneration only works in production
     // reloads data every 30 seconds
     revalidate: 30,
   };
 }
-function HomePage({ products }) {
+function HomePage() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    // need to fire an async function inside useEffect
+    // because useEffect cannot use await directly
+    async function loadProducts() {
+      const products = await getProducts();
+      setProducts(products);
+    }
+    loadProducts();
+  }, []);
   console.log("[HomePage] render", products);
   return (
     <>
