ceae8180774fb64f3933da5a30e8123208460799
Roger Graham <roger.graham@tuni.fi>
2023-10-02 14:07:03 +0300

    Add dynamic single product page

---
+++ pages/products/[id].js
@@ -0,0 +1,37 @@
+import Head from "next/head";
+import Title from "@/components/Title";
+import { getProducts, getProduct } from "@/lib/products";
+export async function getStaticPaths() {
+  const products = await getProducts();
+  return {
+    paths: products.map((product) => ({
+      params: {
+        // path paramenter must be a string
+        id: product.id.toString(),
+      },
+    })),
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params: { id } }) {
+  const product = await getProduct(id);
+  return {
+    props: {
+      product,
+    },
+  };
+}
+
+export default function ProductPage({ product }) {
+  console.log("[ProductPage] render", product);
+  return (
+    <>
+      <Head></Head>
+      <main className="px-6 py-4">
+        <Title>{product.title}</Title>
+        <p>{product.description}</p>
+      </main>
+    </>
+  );
+}
