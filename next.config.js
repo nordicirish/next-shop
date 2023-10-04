5111d2dece8e3408cc7f0c0c5a4c96f873d7154e
Roger Graham <roger.graham@tuni.fi>
2023-10-04 16:13:51 +0300

    Add images domain

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  // allows us to use localhost for images
+  // images are fetched from strapi but served by next
+  images: {
+    domains: ["localhost"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
