a084e998cde60349e11126fd587173791c526f3f
Roger Graham <roger.graham@tuni.fi>
2023-10-04 16:02:40 +0300

    Add product images

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -3,7 +3,7 @@ export default function ProductCard({ product }) {
   return (
     <div className="border w-80 shadow hover:shadow-xl">
       <Link href={`/products/${product.id}`}>
-        <img src="https://dummyimage.com/320x624" alt={product.title} />
+        <img src={product.pictureUrl} alt={product.title} />
         <div className="p-2 flex justify-between items-baseline">
           <h2 className="text-lg font-bold">{product.title}</h2>
           <span>{product.price}</span>
