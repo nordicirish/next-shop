3507a8eda8928a50b9a11b54f7dd73ccf810d3f2
Roger Graham <roger.graham@tuni.fi>
2023-10-05 16:13:26 +0300

    Change title to indoor plants

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ function HomePage({ products }) {
   console.log("[HomePage] render", products);
   return (
     <>
-      <Page title="Home">
+      <Page title="Indoor Plants">
         <ul className="grid grid-cols-1 lg:grid-cols-3 gap-4">
           {products.map((product) => (
             <li key={product.id}>
