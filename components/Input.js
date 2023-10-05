4d4e6aec768a52597c2894dbb330c4e913eaa42d
Roger Graham <roger.graham@tuni.fi>
2023-10-05 15:52:21 +0300

    Add Input component

---
+++ components/Input.js
@@ -0,0 +1,9 @@
+export default function Input({ type }) { 
+  return (
+    <div className="flex flex-col mb-4">
+    
+      <input type={type} className="border rounded px-3 py-1"/>
+    </div>
+  );
+
+}
