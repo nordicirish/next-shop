9bd2dca4f68a1acbc17f003e2884d40b8ed4ce1c
Roger Graham <roger.graham@tuni.fi>
2023-10-08 16:14:47 +0300

    Add QueryClient + QueryClientProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,15 @@
+import { QueryClient, QueryClientProvider } from "react-query";
 import "@/styles/globals.css";
 
+const queryClient = new QueryClient();
+
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  // Wrap the component in the QueryClientProvider
+  // so all components in the app can use react-query
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
 export default App;
