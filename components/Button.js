748fdf3cd11db034aba9cfd8bc96729f071d150f
Roger Graham <roger.graham@tuni.fi>
2023-10-09 13:19:08 +0300

    Add onClick prop

--- components/Button.js
+++ components/Button.js
@@ -1,7 +1,8 @@
-export default function Button({ type, children }) {
+export default function Button({ type, onClick, children }) {
   return (
     <button
       type={type}
+      onClick={onClick}
       className="bg-green-800 hover:bg-green-700 text-gray-100 py-2 px-4 my-2 rounded"
     >
       {children}
