36f07da66e8d3a33db5625213e4849bf9e1b0b1e
Roger Graham <roger.graham@tuni.fi>
2023-10-09 13:03:31 +0300

    Change table format

--- components/CartTable.js
+++ components/CartTable.js
@@ -1,20 +1,54 @@
+//helper function to format currency
+function formatCurrency(value) {
+  return "$" + value.toFixed(2);
+}
+//helper function to build cart totals
+//takes in cartItems array and returns an object with items and total
+function buildCart(cartItems) {
+  let total = 0;
+  const items = [];
+  for (const cartItem of cartItems) {
+    const itemTotal = cartItem.quantity * cartItem.product.price;
+    total += itemTotal;
+    items.push({ ...cartItem, total: itemTotal });
+  }
+  return { items, total };
+}
+
 export default function CartTable({ cartItems }) {
+  const cart = buildCart(cartItems);
   return (
     <table>
       <tr>
         <th className="px-4 py-2">Product</th>
         <th className="px-4 py-2">Price</th>
         <th className="px-4 py-2">Quantity</th>
+        <th className="px-4 py-2">Total</th>
       </tr>
       <tbody>
-        {cartItems.map((cartItem) => (
+        {cart.items.map((cartItem) => (
           <tr key={cartItem.id}>
             <td className="border px-4 py-2">{cartItem.product.title}</td>
-            <td className="border px-4 py-2 text-right">{cartItem.product.price}</td>
+            <td className="border px-4 py-2 text-right">
+              {cartItem.product.price}
+            </td>
             <td className="border px-4 py-2 text-right">{cartItem.quantity}</td>
+            <td className="border px-4 py-2 text-right">
+              {formatCurrency(cartItem.total)}
+            </td>
           </tr>
         ))}
       </tbody>
+      <tfoot>
+        <tr>
+          <th className="border px-4 py-2" colSpan="3">
+            Total
+          </th>
+          <th className="border px-4 py-2 text-right">
+            {formatCurrency(cart.total)}
+          </th>
+        </tr>
+      </tfoot>
     </table>
   );
 }
